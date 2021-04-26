c6e9059a8f7879e47a257abc1c1e433104735b29
Inokcode <regilearn@gmail.com>
2021-04-26 19:11:48 +0530

    Basic react redux pattern commited

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import CakeContainer from "./components/CakeContainer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <CakeContainer />
+      </div>
+    </Provider>
+  );
 }
 
 export default App;
